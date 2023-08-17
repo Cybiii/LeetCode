27188e2de003072ffb0af2a251359836bacc07fc
Sebastian <110711768+Cybiii@users.noreply.github.com>
2023-08-17 02:14:39 -0700

    Time: 41 ms (78.97%), Space: 16.4 MB (38.95%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if divisor == -1 and dividend == -2147483648:
+            return 2147483647
+        elif divisor < 0 or dividend < 0:
+            return int(dividend/divisor)
+        else:
+            return int(dividend/divisor)
